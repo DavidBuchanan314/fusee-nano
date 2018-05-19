33dc2a8ad2c51c016c1d3486e5b552e0f2e41163
DavidBuchanan314 <d@vidbuchanan.co.uk>
2018-05-19 22:25:18 +0100

    refactor out gotos

--- src/usb.c
+++ src/usb.c
@@ -5,34 +5,39 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 #include "usb.h"
 
 #define SYSFS_DEVICES "/sys/bus/usb/devices"
 #define ID_LEN 4
 
 int get_device(int vid, int pid) {
-	DIR *d = opendir(SYSFS_DEVICES);
+	DIR *d;
 	struct dirent *dir;
 	char vid_str[ID_LEN+1];
 	char pid_str[ID_LEN+1];
 	char read_id[ID_LEN+1];
 	char dev_path[PATH_MAX];
 	int fd;
+	bool constraints_match;
 	
 	snprintf(vid_str, sizeof(vid_str), "%04x", vid);
 	snprintf(pid_str, sizeof(pid_str), "%04x", pid);
 	
 	char *constraints[][2] = {
+		/* file name, contents */
 		{"idVendor", vid_str},
 		{"idProduct", pid_str}
 	};
 	
+	d = opendir(SYSFS_DEVICES);
 	if (d) {
 		while ((dir = readdir(d)) != NULL) {
 			if (dir->d_name[0] == '.')
 				continue;
 			
-			for (int i=0; i < (int)(sizeof(constraints) / sizeof(constraints[0])); i++) {
+			constraints_match = true;
+			for (int i = 0; i < (int)(sizeof(constraints) / sizeof(constraints[0])); i++) {
 				snprintf(
 					dev_path,
 					sizeof(dev_path),
@@ -40,40 +45,41 @@ int get_device(int vid, int pid) {
 					dir->d_name,
 					constraints[i][0]);
 				
-				if ((fd = open(dev_path, O_RDONLY)) < 0)
-					goto fail;
+				if ((fd = open(dev_path, O_RDONLY)) < 0) {
+					constraints_match = false;
+					break;
+				}
 				
-				if (read(fd, read_id, ID_LEN) < ID_LEN)
-					goto fail;
-				
-				if (strncmp(read_id, constraints[i][1], ID_LEN) != 0)
-					goto fail;
+				if (read(fd, read_id, ID_LEN) < ID_LEN) {
+					constraints_match = false;
+					close(fd);
+					break;
+				}
 				
 				close(fd);
-				continue;
 				
-			fail:
-				close(fd);
-				goto constraints_failed;
+				if (strncmp(read_id, constraints[i][1], ID_LEN) != 0) {
+					constraints_match = false;
+					break;
+				}
 			}
 			
+			if (!constraints_match)
+				continue;
+			
 			/* if we got this far, the vid/pid matched */
+			closedir(d);
 			snprintf(
-					dev_path,
-					sizeof(dev_path),
-					SYSFS_DEVICES "/%s/descriptors",
-					dir->d_name);
+				dev_path,
+				sizeof(dev_path),
+				SYSFS_DEVICES "/%s/descriptors",
+				dir->d_name);
 			
 			printf("Found device: %s\n", dev_path); // DEBUG
 			
-			closedir(d);
 			return open(dev_path, O_RDONLY);
-			
-		constraints_failed:
-			continue;
 		}
 		closedir(d);
 	}
-	
 	return -1;
 }
